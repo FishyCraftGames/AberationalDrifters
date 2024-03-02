1a27a6bcacda29471161ee85045410b3f4fb4735
qxt2019 <fetiwar@gmail.com>
2024-03-02 22:17:04 +0100

    Car controller

--- Assets/Scripts/Car/car.cs
+++ Assets/Scripts/Car/car.cs
@@ -35,6 +35,12 @@ public class car : MonoBehaviour
     [SerializeField] Transform s2;
     [SerializeField] Transform s3;
     [SerializeField] Transform s4;
+
+    [SerializeField] Transform w1;
+    [SerializeField] Transform w2;
+    [SerializeField] Transform w3;
+    [SerializeField] Transform w4;
+
     public float lastoffsets1;
     public float lastoffsets2;
     public float lastoffsets3;
@@ -43,6 +49,8 @@ public class car : MonoBehaviour
     [Space]
     [SerializeField] Rigidbody rb;
 
+    public bool isGrounded;
+
     void Start()
     {
         rb = GetComponent<Rigidbody>();
@@ -50,30 +58,40 @@ public class car : MonoBehaviour
 
     void FixedUpdate()
     {
+        isGrounded = false;
+
         Debug.Log(rb.velocity.magnitude * 3.6f);
 
-        accelInput = Mathf.Clamp((Input.GetAxis("Vertical") + (Input.GetAxis("R2")+1) - (Input.GetAxis("L2")+1)), -1, 1) * 50000;
+        accelInput = Mathf.Clamp((Input.GetAxis("Vertical") + (Input.GetAxis("R2")+1) - (Input.GetAxis("L2")+1)), -1, 1) * 35000;
 
         bool handbrake = false;
-        if (Input.GetKey(KeyCode.Space) || Input.GetButton("Fire2"))
+        if (Input.GetKey(KeyCode.Space) || Input.GetButton("Fire1"))
             handbrake = true;
 
-        lastoffsets1 = Spring(s1, lastoffsets1, accelInput, frontStrength, FrontGrip, handbrake);
-        lastoffsets2 = Spring(s2, lastoffsets2, accelInput, frontStrength, FrontGrip, handbrake);
-        lastoffsets3 = Spring(s3, lastoffsets3, accelInput, backStrength, BackGrip, handbrake);
-        lastoffsets4 = Spring(s4, lastoffsets4, accelInput, backStrength, BackGrip, handbrake);
+        lastoffsets1 = Spring(s1, lastoffsets1, accelInput, frontStrength, FrontGrip, false, w1);
+        lastoffsets2 = Spring(s2, lastoffsets2, accelInput, frontStrength, FrontGrip, false, w2);
+        lastoffsets3 = Spring(s3, lastoffsets3, accelInput, backStrength, BackGrip, handbrake, w3);
+        lastoffsets4 = Spring(s4, lastoffsets4, accelInput, backStrength, BackGrip, handbrake, w4);
 
         float steering = Input.GetAxis("Horizontal") * -25 * steeringSensitivity;
 
         s1.localRotation = Quaternion.Euler(s1.localEulerAngles.x, -steering, s1.localEulerAngles.z);
         s2.localRotation = Quaternion.Euler(s2.localEulerAngles.x, -steering, s2.localEulerAngles.z);
+
+        w1.localRotation = Quaternion.Euler(s1.localEulerAngles.x, -steering, s1.localEulerAngles.z);
+        w2.localRotation = Quaternion.Euler(s2.localEulerAngles.x, -steering, s2.localEulerAngles.z);
     }
 
-    float Spring(Transform a, float b, float c, float strength, AnimationCurve tireGrip, bool handbreak)
+    float Spring(Transform a, float b, float c, float strength, AnimationCurve tireGrip, bool handbreak, Transform wheel)
     {
         Ray ray = new Ray(a.position, -a.up);
         if (Physics.Raycast(ray, out RaycastHit hit))
         {
+            if(hit.distance < length)
+            {
+                isGrounded = true;
+            }
+
             //Springs
 
             float offset = length - Mathf.Clamp(hit.distance, 0, length);
@@ -82,6 +100,10 @@ public class car : MonoBehaviour
             float force = (offset * strength) - (velocity * dampening);
             //rb.AddForceAtPosition(new Vector3(0, force, 0), a.position, ForceMode.Acceleration);
             rb.AddForceAtPosition(transform.up * force, a.position, ForceMode.Acceleration);
+
+            //update tire position
+            Vector3 tirePos = a.position + -a.up * Mathf.Clamp(hit.distance, 0.22f, 0.8f) + a.up * 0.35f;
+            wheel.position = tirePos;
 
             //Steering
             Vector3 steeringDir = a.right;
@@ -92,7 +114,7 @@ public class car : MonoBehaviour
             float tireGripPercent = 100 / tireVel.magnitude * steeringVel;
 
             float velChange = 0;
-            if (!handbreak) 
+            if (true) 
             {
                 velChange = -steeringVel * tireGrip.Evaluate(tireGripPercent);
             }
@@ -105,25 +127,39 @@ public class car : MonoBehaviour
             rb.AddForceAtPosition(steeringDir * TireMass * velAcceleration, a.position);
 
             //Driving
-            if (c > 0f)
+            if (!handbreak)
             {
-                Vector3 accelDir = a.forward;
+                if (c > 0f)
+                {
+                    Vector3 accelDir = a.forward;
 
-                float carSpeed = Vector3.Dot(transform.forward, rb.velocity);
-                float normalizeSpeed = Mathf.Clamp01(Mathf.Abs(carSpeed) / carTopSpeed);
-                float avaliableTorque = powerCurve.Evaluate(normalizeSpeed) * c * 2;
+                    float carSpeed = Vector3.Dot(transform.forward, rb.velocity);
+                    float normalizeSpeed = Mathf.Clamp01(Mathf.Abs(carSpeed) / carTopSpeed);
+                    float avaliableTorque = powerCurve.Evaluate(normalizeSpeed) * c * 2;
 
-                rb.AddForceAtPosition(accelDir * avaliableTorque, a.position);
-            }
-            else if (c < 0f)
-            {
-                Vector3 accelDir = -a.forward;
+                    rb.AddForceAtPosition(accelDir * avaliableTorque, a.position);
+                }
+                else if (c < 0f)
+                {
+                    Vector3 accelDir = -a.forward;
 
-                float carSpeed = Vector3.Dot(transform.forward, rb.velocity);
-                float normalizeSpeed = Mathf.Clamp01(Mathf.Abs(carSpeed) / carTopSpeed);
-                float avaliableTorque = powerCurve.Evaluate(normalizeSpeed) * Mathf.Abs(c / 2);
+                    float carSpeed = Vector3.Dot(transform.forward, rb.velocity);
+                    float normalizeSpeed = Mathf.Clamp01(Mathf.Abs(carSpeed) / carTopSpeed);
+                    float avaliableTorque = powerCurve.Evaluate(normalizeSpeed) * Mathf.Abs(c / 2);
 
-                rb.AddForceAtPosition(accelDir * avaliableTorque, a.position);
+                    rb.AddForceAtPosition(accelDir * avaliableTorque, a.position);
+                }
+                else
+                {
+                    Vector3 accelDir = a.forward;
+                    Vector3 tireVel2 = rb.GetPointVelocity(a.position);
+
+                    float steeringVel2 = Vector3.Dot(accelDir, tireVel2);
+                    float velChange2 = -steeringVel2 * rollGrip;
+                    float velAcceleration2 = velChange2 / Time.fixedDeltaTime;
+
+                    rb.AddForceAtPosition(accelDir * TireMass * velAcceleration2, a.position);
+                }
             }
             else
             {
@@ -131,11 +167,11 @@ public class car : MonoBehaviour
                 Vector3 tireVel2 = rb.GetPointVelocity(a.position);
 
                 float steeringVel2 = Vector3.Dot(accelDir, tireVel2);
-                float velChange2 = -steeringVel2 * rollGrip;
+                float velChange2 = -steeringVel2 * 1;
                 float velAcceleration2 = velChange2 / Time.fixedDeltaTime;
 
                 rb.AddForceAtPosition(accelDir * TireMass * velAcceleration2, a.position);
-            } 
+            }
 
             return (offset);
         }
--- Assets/Scripts/FollowCam.cs
+++ Assets/Scripts/FollowCam.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class FollowCam : MonoBehaviour
 {
@@ -8,10 +9,19 @@ public class FollowCam : MonoBehaviour
     public Transform target;
     public float speed = 0.25f;
     public float MinDist;
+    public Transform cam;
+
+    public float screenshakePosStrength = 0.3f;
+    public float screenshakeRotStrength;
+
+    public GameObject volume;
+    public LayerMask mask;
+
+    private Vector3 lastTarget;
 
     void FixedUpdate()
     {
-        Physics.Raycast(target.position + Vector3.up * 500f, Vector3.down, out RaycastHit TargetRay);
+        Physics.Raycast(target.position + Vector3.up * 500f, Vector3.down, out RaycastHit TargetRay, 999f, mask);
         float offset = (target.position + Vector3.up * 2.4f - TargetRay.point).y;
         if (offset > MinDist)
         {
@@ -25,6 +35,21 @@ public class FollowCam : MonoBehaviour
         Vector3 speedDir = target.root.GetComponent<Rigidbody>().velocity;
         if (speedDir.magnitude > 3)
             speedDir = speedDir.normalized * 3;
-        transform.LookAt(target.root.position + speedDir);
+        transform.LookAt(Vector3.Lerp(lastTarget, target.root.position + speedDir, 0.25f));
+        lastTarget = target.root.position + speedDir;
+
+        //post-processing
+        volume.GetComponent<UnityEngine.Rendering.Volume>().weight = target.root.GetComponent<Rigidbody>().velocity.magnitude / 260;
+
+        //camera shake
+        if (target.root.GetComponent<Player>().activeCar.isGrounded)
+        {
+            Vector3 randomPos = new Vector3(Random.Range(-1f, 1f), Random.Range(-1f, 1f), Random.Range(-1f, 1f)) * screenshakePosStrength * (target.root.GetComponent<Rigidbody>().velocity.magnitude / 260);
+            randomPos.z = 0;
+            cam.transform.localPosition = randomPos;
+
+            Vector3 randomRot = new Vector3(Random.Range(-1f, 1f), Random.Range(-1f, 1f), Random.Range(-1f, 1f)) * screenshakeRotStrength * (target.root.GetComponent<Rigidbody>().velocity.magnitude / 260);
+            cam.localEulerAngles = randomRot;
+        }
     }
 }
